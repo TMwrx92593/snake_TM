b18eeef54b6c2142c87b9f1a68bf801066507133
Tomasz <wrx92593@student.wroclaw.merito.pl>
2025-04-03 20:37:07 +0200

    Główna funkcja gry

--- Snake_TM.py
+++ Snake_TM.py
@@ -33,3 +33,40 @@ def pokaz_wynik(wynik):
 def rysuj_weza(wielkosc_segmentu, lista_segmentów):
     for x in lista_segmentów:
         pygame.draw.rect(okno, zielony, [x[0], x[1], wielkosc_segmentu, wielkosc_segmentu])
+
+def gra():
+    game_over = False
+    game_close = False
+
+    
+    x1 = szerokość / 2
+    y1 = wysokość / 2
+
+    x1_zmiana = 0
+    y1_zmiana = 0
+
+  
+    segmenty_węża = []
+    dlugosc_węża = 1
+
+  
+    jabłko_x = round(random.randrange(0, szerokość - wielkosc_segmentu) / 10.0) * 10.0
+    jabłko_y = round(random.randrange(0, wysokość - wielkosc_segmentu) / 10.0) * 10.0
+
+    while not game_over:
+
+        while game_close:
+            okno.fill(niebieski)
+            pokaz_wynik(dlugosc_węża - 1)
+            pygame.display.update()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    game_over = True
+                    game_close = False
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        game_over = True
+                        game_close = False
+                    if event.key == pygame.K_c:
+                        gra()
