21fe8bd2ac9d5554f3c6b1717403763c2087206b
Tomasz <wrx92593@student.wroclaw.merito.pl>
2025-04-09 19:04:23 +0200

    poprawki_finalizacja

--- Snake_TM.py
+++ Snake_TM.py
@@ -17,8 +17,6 @@ pygame.display.set_caption('Snake Game')
 
 clock = pygame.time.Clock()
 szybkość = 10 
-
-
 wielkosc_segmentu = 10
 
 font_style = pygame.font.SysFont("bahnschrift", 25)
@@ -26,64 +24,66 @@ wynik_font = pygame.font.SysFont("comicsansms", 35)
 
 
 def pokaz_wynik(wynik):
+    """Wyświetla aktualny wynik w lewym górnym rogu."""
     value = wynik_font.render(f"Twoje wyniki: {wynik}", True, czarny)
-    okno.blit(value, [0, 0])
+    okno.blit(value, (0,0))
 
 
-def rysuj_weza(wielkosc_segmentu, lista_segmentów):
-    for x in lista_segmentów:
-        pygame.draw.rect(okno, zielony, [x[0], x[1], wielkosc_segmentu, wielkosc_segmentu])
+def rysuj_weza( lista_segmentów):
+    """Rysuje węża na ekranie."""
+    for segment in lista_segmentów:
+        pygame.draw.rect(okno, zielony, [segment[0], segment[1], wielkosc_segmentu, wielkosc_segmentu])
 
 
 
 def ekran_powitalny():
+    """Wyświetla ekran powitalny przed rozpoczęciem gry."""
     okno.fill(niebieski)
     powitanie_text = font_style.render("Snake let's play - Push the button", True, czarny)
+    instrukcja_text = font_style.render("Naciśnij dowolny klawisz, aby rozpocząć!", True, czarny)
+
     powitanie_x = (szerokość - powitanie_text.get_width()) / 2  
     powitanie_y = wysokość / 2 - 25  
-    okno.blit(powitanie_text, [powitanie_x, powitanie_y])
-
- 
-    instrukcja_text = font_style.render("Naciśnij dowolny klawisz, aby rozpocząć!", True, czarny)
     instrukcja_x = (szerokość - instrukcja_text.get_width()) / 2  
     instrukcja_y = wysokość / 2 + 25 
-    okno.blit(instrukcja_text, [instrukcja_x, instrukcja_y])
 
+    okno.blit(powitanie_text, (powitanie_x, powitanie_y))
+    okno.blit(instrukcja_text, (instrukcja_x, instrukcja_y))
     pygame.display.update()
 
 def rysuj_obramowanie():
+
     cegła_szerokość = 20  
     cegła_wysokość = 10  
-
     for x in range(0, szerokość, cegła_szerokość):
-        pygame.draw.rect(okno, cegla, [x, 0, cegła_szerokość, cegła_wysokość])
-        pygame.draw.rect(okno, cegla, [x, wysokość - cegła_wysokość, cegła_szerokość, cegła_wysokość]) 
-    
+        pygame.draw.rect(okno, cegla, (x, 0, cegła_szerokość, cegła_wysokość))
+        pygame.draw.rect(okno, cegla, (x, wysokość - cegła_wysokość, cegła_szerokość, cegła_wysokość)) 
     for y in range(0, wysokość, cegła_wysokość):
-        pygame.draw.rect(okno, cegla, [0, y, cegła_szerokość, cegła_wysokość])  
-        pygame.draw.rect(okno, cegla, [szerokość - cegła_szerokość, y, cegła_szerokość, cegła_wysokość])  
+        pygame.draw.rect(okno, cegla, (0, y, cegła_szerokość, cegła_wysokość))  
+        pygame.draw.rect(okno, cegla, (szerokość - cegła_szerokość, y, cegła_szerokość, cegła_wysokość))  
+
 
 def generuj_jablko():
  
-    safe_zone_start = 20 
-    safe_zone_end_x = szerokość - 20  
-    safe_zone_end_y = wysokość - 20  
+    margin = 20 
+    safe_zone_end_x = szerokość - margin  
+    safe_zone_end_y = wysokość - margin
 
-    jabłko_x = round(random.randrange(safe_zone_start, (safe_zone_end_x // wielkosc_segmentu)) - 1) * wielkosc_segmentu
-    jabłko_y = round(random.randrange(safe_zone_start, (safe_zone_end_y // wielkosc_segmentu)) - 1) * wielkosc_segmentu
+    jabłko_x = round(random.randrange(margin, (safe_zone_end_x // wielkosc_segmentu)) - 1) * wielkosc_segmentu
+    jabłko_y = round(random.randrange(margin, (safe_zone_end_y // wielkosc_segmentu)) - 1) * wielkosc_segmentu
     return jabłko_x, jabłko_y
 
 def pokaz_game_over(wynik):
     game_over_text = font_style.render("Game Over!", True, czarny)
     wynik_text = font_style.render(f"Twój wynik: {wynik}", True, czarny)
     
-    tekst_x = (szerokość - game_over_text.get_width()) / 2
-    tekst_y = wysokość / 2 - 50
-    okno.blit(game_over_text, [tekst_x, tekst_y])
-
+    game_over_x  = (szerokość - game_over_text.get_width()) / 2
+    game_over_y  = wysokość / 2 - 50
     wynik_x = (szerokość - wynik_text.get_width()) / 2
     wynik_y = wysokość / 2
-    okno.blit(wynik_text, [wynik_x, wynik_y])
+
+    okno.blit(game_over_text, (game_over_x , game_over_y))
+    okno.blit(wynik_text, (wynik_x, wynik_y))
 
 
 def gra():
@@ -105,7 +105,6 @@ def gra():
     jabłko_x, jabłko_y = generuj_jablko()
 
     pierwszy_ruch = False
-
     ostatni_kierunek = None
 
     while not game_over:
@@ -131,11 +130,11 @@ def gra():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game_over = True
+
+
             if event.type == pygame.KEYDOWN:
                 if not pierwszy_ruch:
                     pierwszy_ruch = True 
-
-                
                 if event.key == pygame.K_LEFT and ostatni_kierunek != "RIGHT":
                     x1_zmiana = -wielkosc_segmentu
                     y1_zmiana = 0
@@ -154,6 +153,7 @@ def gra():
                     ostatni_kierunek = "DOWN"
 
         if not pierwszy_ruch:
+            clock.tick(szybkość)
             continue
 
         if x1 >= szerokość - wielkosc_segmentu or x1 < wielkosc_segmentu or y1 >= wysokość - wielkosc_segmentu or y1 < wielkosc_segmentu:
@@ -161,26 +161,24 @@ def gra():
 
         x1 += x1_zmiana
         y1 += y1_zmiana
+
         okno.fill(niebieski)
-
         rysuj_obramowanie()
-
         pygame.draw.rect(okno, czerwony, [jabłko_x, jabłko_y, wielkosc_segmentu, wielkosc_segmentu])
 
-        segment_węża = []
-        segment_węża.append([x1, y1])
+        glowa = [x1, y1]
+        segmenty_węża.append(glowa)
         if len(segmenty_węża) > dlugosc_węża:
             del segmenty_węża[0]
 
-        segmenty_węża.append(segment_węża[0])
+       
 
-        for x in segmenty_węża[:-1]:
-            if x == segment_węża[0]:
+        for segment in segmenty_węża[:-1]:
+            if segment == glowa:
                 game_close = True
 
-        rysuj_weza(wielkosc_segmentu, segmenty_węża)
+        rysuj_weza(segmenty_węża)
         pokaz_wynik(dlugosc_węża - 1)
-
         pygame.display.update()
 
 
@@ -207,4 +205,5 @@ def menu():
                 rozpocznij_grę = True
                 gra()
 
-menu()
+if __name__ == '__main__':
+    menu()
